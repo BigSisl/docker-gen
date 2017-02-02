15b5e7aa5bce7cacc46ebcd9bfe433dbe37555c1
Lucien Zürcher <l.zuercher@tincan.ch>
2017-02-02 16:05:07 +0100

    test: testing

--- template.go
+++ template.go
@@ -489,13 +489,13 @@ func GenerateFile(config Config, containers Context) bool {
 	t := filteredContainers
 	s := reflect.ValueOf(&t).Elem()
 	typeOfT := s.Type();
-
-/*	for i := 0; i < s.NumField(); i++ {
+/*
+	for i := 0; i < s.NumField(); i++ {
 		f := s.Field(i)
 		fmt.Printf("%d: %s %s = %v\n", i,
 			typeOfT.Field(i).Name, f.Type(), f.Interface())
-	}
-*/
+	}*/
+
 	contents := executeTemplate(config.Template, filteredContainers)
 
 	if !config.KeepBlankLines {
